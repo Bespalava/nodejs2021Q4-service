33930bee5e9fd48eaca23855bdaf6459a90468fb
ybespalava <ybespalava@gmail.com>
2021-12-03 19:34:22 +0300

    feat: board model

---
+++ src/resources/board/board.model.js
@@ -0,0 +1,17 @@
+const uuid = require('uuid').v4;
+const Column = require('./columns.model');
+
+class Board {
+  constructor({id = uuid(),title = 'board',columns = [],} = {}) {
+    this.id = id;
+    this.title = title;
+    this.columns = columns.map((col) => new Column(col));
+  }
+
+  static toResponse(board) {
+    const { id, title, columns } = board;
+    return { id, title, columns };
+  }
+}
+
+module.exports = Board;
